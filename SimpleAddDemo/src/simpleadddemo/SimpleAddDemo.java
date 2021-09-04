9e69eaa2ac35ae3b6b8ac28bc86efa750bfd1e95
Nasim-Hasan <nasimcse2k5@gmail.com>
2021-09-05 01:50:05 +0600

    Initial Commit

---
+++ SimpleAddDemo/src/simpleadddemo/SimpleAddDemo.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package simpleadddemo;
+
+/**
+ * @author nhasa
+ *
+ */
+public class SimpleAddDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a=10,b=30,result=0;
+      
+      result=a+b;
+      
+      System.out.println("Result is:"+result);
+		
+		
+		
+	}
+
+}
