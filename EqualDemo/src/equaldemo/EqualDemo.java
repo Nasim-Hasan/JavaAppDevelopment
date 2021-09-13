a210b02041cb2b0df1f76fa23bcc234b17039c78
Nasim-Hasan <nasimcse2k5@gmail.com>
2021-09-13 21:55:13 +0600

    Equal Demo Project Commit

---
+++ EqualDemo/src/equaldemo/EqualDemo.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package equaldemo;
+
+/**
+ * @author nhasa
+ *
+ */
+public class EqualDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double result=0.0, result1=0.0;
+		
+		result= 4.0/3.0*12.0;
+		result1= (4.0*12.0)/3.0;
+		
+		if(result==result1)
+		{
+			System.out.println("The Expressions Are Equal");
+		}
+		else
+		{
+			System.out.println("The Expressions Are Not Equal");
+		}
+		
+
+	}
+
+}
