ec6dbe256642e808710092e511c30df920cb522f
Nasim-Hasan <nasimcse2k5@gmail.com>
2021-12-31 16:08:47 +0600

    Void Return Demo

---
+++ VoidReturnDemo/src/voidreturndemo/VoidReturnDemo.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package voidreturndemo;
+
+/**
+ * @author nhasa
+ *
+ */
+public class VoidReturnDemo {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void doCompare(int num)
+	{
+		if(num>10)
+		{
+			System.out.print("Number is Greater Than 10");
+			return;
+		}
+		else
+		{
+			num++;
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		doCompare(12);
+	}
+
+}
