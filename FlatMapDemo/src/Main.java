86d488a5538d707b648ab1337cc113b62e37a65a
Nasim-Hasan <nasimcse2k5@gmail.com>
2025-03-13 21:41:32 +0600

    Adding New Java Source Codes about the Flat Map

---
+++ FlatMapDemo/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // Creating a List of Lists
+        List<List<String>> listOfLists = Arrays.asList(
+                Arrays.asList("Geeks", "For"),
+                Arrays.asList("GeeksForGeeks", "A computer portal"),
+                Arrays.asList("Java", "Programming")
+        );
+
+        // Using Stream flatMap(Function mapper)
+        listOfLists.stream()
+                .flatMap(list -> list.stream())
+                .forEach(System.out::println);
+    }
+}
