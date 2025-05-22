5227ca6047ed5b53c70f7ea9e9bca419baa8c3f2
Nasim-Hasan <nasimcse2k5@gmail.com>
2025-05-22 21:52:51 +0600

    New Java Codes about Virtual Thread

---
+++ VirtualThreadExecutorExample/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) {
+        try (ExecutorService myExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
+            // Submit a task that prints a message
+            Future<?> future = myExecutor.submit(() -> System.out.println("Running thread"));
+
+            // Wait for the task to complete
+            future.get();
+            System.out.println("Program Completed !!");
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
